c98680e8f80d1f2ba5213b3d5a1727702e8ea2f4
Daniel Clavijo <dclavijo@tec.mx>
2023-11-02 18:30:37 -0600

    update

--- source/conf.py
+++ source/conf.py
@@ -13,6 +13,7 @@ release = '1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 extensions = ['sphinxcontrib.icon']
+html_static_path = ['_static']
 templates_path = ['_templates/']
 exclude_patterns = ['book/*']
 
@@ -20,9 +21,6 @@ exclude_patterns = ['book/*']
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 html_theme = 'press'
 html_title = 'd3040'
-html_favicon = '☕︎'
-
-html_static_path = ['_static']
 html_css_files = ['css/custom.css']
 #html_js_files = ['js/custom.js']
 #html_theme_options = {"external_links": [("Github", "https://github.com/d3040/the_beavers_hut"),("d3040", "https://d3040.com")]}
