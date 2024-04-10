0c33548e3ab1137856fc9633e2b90148ea726727
d3040 <daniel.clavijo@pm.me>
2024-04-10 16:24:19 -0600

    workflow

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,7 +31,7 @@ copyright = today_year + ', Daniel Clavijo'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'pydata_sphinx_theme'
+    #'pydata_sphinx_theme'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -72,14 +72,13 @@ html_sidebars =  {
     #"max_navbar_depth": 4,
     #"collapse_navbar": False,#True,
 }
+html_title = project
+html_logo = '_static/img/beaver.png'
+html_favicon = '_static/img/accessibility.svg'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 html_css_files = ['css/custom.css']
 html_js_files = ['js/custom.js']
-html_title = project
-html_logo = 'img/beaver.png'
-html_favicon = 'img/accessibility.svg'
-
 
