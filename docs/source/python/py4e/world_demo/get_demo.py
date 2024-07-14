06c6ec767b816dde984553df4c3c5b052a89a77a
Daniel Clavijo <dclavijo@tec.mx>
2024-07-14 10:45:20 -0600

    gitignore update test

--- docs/source/python/py4e/world_demo/get_demo.py
+++ docs/source/python/py4e/world_demo/get_demo.py
@@ -21,24 +21,95 @@ url = 'https://en.wikipedia.org/wiki/List_of_sovereign_states'
 # crawling foreing countries
 site = urlopen(url, context=ctx)
 html = site.read()
-# catch error? // TBD
+# transactional dictionary
+demos = dict()
+# get structure of page (bs4 object)
+# get countries info from list of sovereing states
 html_parser = BeautifulSoup(html, 'html.parser')
-a_tags = html_parser.select('table>tbody>tr>td>b>a')
+table_rows = html_parser.select('table.wikitable>tbody>tr')
+for n, row in enumerate(table_rows):
+    # discard table headers
+    country_info = row.find_all('td')
+    if country_info:
+        country = country_info[0].a['title']
+        wiki_ref = country_info[0].a['href']
+        # get UN membership
+        membership = country_info[1].get_text()[2:].lower()
+        if 'no membership' in membership:
+            membership = 'No membership'
+        elif 'un general assembly observer state' in membership:
+            membership = 'UN General Assembly observer state'
+        elif 'un specialized agencies' in membership:
+            membership = 'UN specialized agencies'
+        elif 'un member state' in membership:
+            membership = 'UN member state'
+        else:
+            membership = 'Not specified'
+        # save transaction
+        demos[country] = {'url': domain + wiki_ref,  'un_membership': membership}
+# get demographics
+for country, info in demos.items():
+    url = info['url']
+    site = urlopen(url, context=ctx)
+    html = site.read()
+    # if error found when opening page, update ERROR on page
+    if site.getcode() != 200 :
+        print('Error on page (' + url + '): ',document.getcode())
+    # get structure of page (bs4 object)
+    html_parser = BeautifulSoup(html, 'html.parser')
+    table_rows = html_parser.select('table.infobox>tbody>tr')
+    fields = list()
+    values = list()
+    for row in table_rows:
+        infobox_label = row.find('th', class_='infobox-label')
+        infobox_data = row.find('td', class_='infobox-data')
+        if infobox_label and infobox_data:
+            field = infobox_label.get_text('|', strip=True)
+            value = infobox_data.get_text('|', strip=True)
+            print(field,((40 - len(field)) * ' ') + '|',value)
+            wait = input('-'*120)
+            #fields.append(infobox_label.get_text('|', strip=True))
+            #values.append(infobox_data.get_text('|', strip=True))
+    #print('Keys:\n', fields)
+    #print('Values:\n', values)
+    #wait = input('Press any key to continue')
+'''
+# TBD
+# catch error? // TBD
 # create db
 conn = sqlite3.connect('world_demo.sqlite')
 cur = conn.cursor()
+cur.executescript(''''''
+                CREATE TABLE IF NOT EXISTS Membership
+                    (id INTEGER,
+                    name TEXT UNIQUE,
+                    PRIMARY KEY ("id" AUTOINCREMENT));
 
-cur.execute('''CREATE TABLE IF NOT EXISTS Countries
-                (id INTEGER,
-                name TEXT UNIQUE,
-                PRIMARY KEY ("id" AUTOINCREMENT))''')
+                CREATE TABLE IF NOT EXISTS Countries
+                    (id INTEGER,
+                    name TEXT UNIQUE,
+                    url TEXT UNIQUE,
+                    PRIMARY KEY ("id" AUTOINCREMENT));
 
-# get states
-n = 0
-for a in a_tags:
-    country_name = a['title'] # + ' -> ' + domain + a['href'])
-    # smart db update // TBD
-    cur.execute('INSERT OR IGNORE INTO Countries (name) VALUES ( ? )', ( country_name, ))
+                CREATE TABLE IF NOT EXISTS Demos
+                    (id INTEGER,
+                    id_country INTEGER,
+                    id_un_membership INTEGER,
+                    PRIMARY KEY ("id" AUTOINCREMENT),
+                    FOREIGN KEY (id_un_membership) REFERENCES Membership(id));
+                  '''''')
+# insert country and UN membership status in db
+cur.execute('INSERT OR IGNORE INTO Countries (name, url) VALUES (?, ?)', (country, wiki_ref))
+cur.execute('SELECT id FROM Membership WHERE name = ?', (membership, ))
+if cur.fetchone() is None:
+cur.execute('INSERT INTO Membership (name) VALUES (?)', (membership, ))
+cur.execute('SELECT id FROM Membership WHERE name = ?', (membership, ))
+id_un_membership = cur.fetchone()[0]
+cur.execute('SELECT id FROM Countries WHERE name = ?', (country, ))
+id_country = cur.fetchone()[0]
+ # smart db update // TBD
     if n % 25 == 0: conn.commit()
-    n += 1
+# final commit
+conn.commit()
+'''
 
